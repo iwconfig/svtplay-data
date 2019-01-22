209e77cc9f3f3aea237cafce15d2328da1c6af33
iwconfig <snelhingst@gmail.com>
2019-01-22 18:31:07 +0100

    Fix credentials

--- cronjob.sh
+++ cronjob.sh
@@ -5,14 +5,21 @@
 ##
 
 TMPDIR=/tmp/svtplay-data
+USERNAME='your username'
+PASSWORD='your password'
+EMAIL='your email'
 
 mkdir -p $TMPDIR
 cd $TMPDIR
 
 git clone https://github.com/iwconfig/svtplay-data .
 
+git config user.email $EMAIL
+git config user.name $USERNAME
+
 if ./gather_data.py; then
-    git commit -a -m "Daily data update: $(date '+%Y-%m-%d %H:%M:%S')"
-    git push -u origin master
+    git add *
+    git commit -m "Daily data update: $(date '+%Y-%m-%d %H:%M:%S')"
+    git push -u https://$USERNAME:$PASSWORD@github.com/$USERNAME/svtplay-data master
     rm -rf $TMPDIR
 fi
