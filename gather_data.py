1d0b2546c971bddf4d8320901bf6587f777ea6da
iwconfig <snelhingst@gmail.com>
2019-01-23 18:50:04 +0100

    backup title page data also (if available)

--- gather_data.py
+++ gather_data.py
@@ -24,35 +24,48 @@ def stream_handler(data):
     for dct in data:
         yield dct
 
-data = []
+def json_cleanup(data):
+    for x in 'message', 'messages':
+        del data[x]    
+    return data
+
+singles_and_episodes = []
+title_pages = []
 for d in svtapi.all_titles_and_singles():
     try:
         dct = svtapi.episode_info(d['contentUrl'])
-        for x in 'message', 'messages':
-            del dct[x]
-        data.append(dct)
+        singles_and_episodes.append(json_cleanup(dct))
     except svtapi.ParameterNotFound:
         dctlist = svtapi.all_episodes_info_by_title(d['contentUrl'])
         for dct in dctlist:
-            for x in 'message', 'messages':
-                del dct[x]
-        data.extend(dctlist)
+            dct = json_cleanup(dct)
+        singles_and_episodes.extend(dctlist)
+    try: 
+        dct = svtapi.program_info_by_slug(d['contentUrl'])
+        title_pages.append(json_cleanup(dct))
+    except svtapi.JSONResponseEmpty: 
+        pass
+        
+data = [
+    (singles_and_episodes, Path('./singles_and_episodes')),
+    (title_pages, Path('./title_pages'))
+]
 
-datafile = Path('./singles_and_episodes')
+for data, datafile in data:
+    if datafile.is_file():
+        datafile.rename(datafile.with_suffix('.bak'))
 
-if datafile.is_file():
-    datafile.rename(datafile.with_suffix('.bak'))
+        with datafile.with_suffix('.bak').open() as bakfile:
+            bakdata = json.load(bakfile)
 
-    with datafile.with_suffix('.bak').open() as bakfile:
-        bakdata = json.load(bakfile)
+        data.extend(bakdata)
+        data = list({v['id']:v for v in data}.values())
 
-    data.extend(bakdata)
-    data = list({v['id']:v for v in data}.values())
+    with datafile.open('x') as outfile:
+        stream_array = StreamArray(stream_handler(data))
+        for dct in json.JSONEncoder(indent=2, ensure_ascii=False, sort_keys=True).iterencode(stream_array):
+            outfile.write(dct)
 
-with datafile.open('x') as outfile:
-    stream_array = StreamArray(stream_handler(data))
-    for dct in json.JSONEncoder(indent=2, ensure_ascii=False, sort_keys=True).iterencode(stream_array):
-        outfile.write(dct)
+    if datafile.with_suffix('.bak').is_file():
+        datafile.with_suffix('.bak').unlink()
 
-if datafile.with_suffix('.bak').is_file():
-    datafile.with_suffix('.bak').unlink()
