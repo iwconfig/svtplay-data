032b6ad599789ba322b0e4d8f58883beff1a337f
iwconfig <snelhingst@gmail.com>
2019-01-23 18:50:52 +0100

    prevent crontab from launching multiple instances

--- cronjob.sh
+++ cronjob.sh
@@ -1,14 +1,65 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 ## Add this to crontab with crontab -e
 ## * */6 * * * /PATH/TO/cronjob.sh
 ##
+
+LOCKDIR=/tmp/svtplay-data.lock
+PIDFILE=$LOCKDIR/pid
+LOGFILE=/tmp/svtplay-data.log
 
 TMPDIR=/tmp/svtplay-data
 USERNAME='your username'
 PASSWORD='your password'
 EMAIL='your email'
 
+# make echo act as a logger
+function echo() {
+    msg="$(date '+%Y-%m-%d %H:%M:%S'): $@"
+    builtin echo "$msg" >> $LOGFILE
+    builtin echo -e "\r$msg"
+}
+
+# trap cleanup
+function cleanup {
+    echo "Removing pid file '$PIDFILE'"
+    if ! rm $LOCKDIR/pid 2>/dev/null; then
+	echo "Failed to remove pid file '$PIDFILE'"
+        exit 1
+    fi
+    echo "Removing lock directory '$LOCKDIR'"
+    if ! rmdir $LOCKDIR; then
+        echo "Failed to remove lock directory '$LOCKDIR'"
+        exit 1
+    fi
+    echo "Removing log file '$LOGFILE'"
+    rm $LOGFILE
+    exit 0
+}
+
+# check locker
+if mkdir $LOCKDIR 2>/dev/null; then
+    cat <<< $$ > $PIDFILE
+    echo PID: $(cat $PIDFILE)
+    trap "cleanup" EXIT
+    echo "Acquired lock, running"
+else
+    echo "Could not create lock directory '$LOCKDIR'."
+    if [ -f $PIDFILE ] && kill -0 $(cat $PIDFILE) 2>/dev/null; then	
+	echo "Process is running with PID $(cat $PIDFILE)."
+    else
+	if [ -w $LOCKDIR ]; then
+	    echo "The lock already exists, but no process is running..."
+	    echo "Trying to remove and restart. If no success, then try to remove it manually."
+	    trap "cleanup; $0" EXIT	    
+	else
+	    echo "No write access!"
+	fi
+    fi
+    exit 1
+fi
+
+# main
 mkdir -p $TMPDIR
 cd $TMPDIR
 
@@ -18,8 +69,10 @@ git config user.email $EMAIL
 git config user.name $USERNAME
 
 if ./gather_data.py; then
+    echo "Data gathering went fine. Now making commit and pushing to github."
     git add *
     git commit -m "Daily data update: $(date '+%Y-%m-%d %H:%M:%S')"
     git push -u https://$USERNAME:$PASSWORD@github.com/$USERNAME/svtplay-data master
     rm -rf $TMPDIR
+    exit 0
 fi
